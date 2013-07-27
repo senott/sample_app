accdefbe6552b7dc7d9ecde5241966e3bdc9a74b
Senott <senott@yahoo.com>
2013-07-27 12:54:05 -0300

    Home and Help pages test is running

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,8 +6,18 @@ describe "Static pages" do
     
     it "should have the contant 'Sample App'" do
       
-      visit 'static_pages/home'
+      visit '/static_pages/home'
       expect(page).to have_content ('Sample App')
     end
   end
+  
+  describe "Help page" do
+    
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+    
+  end
+  
 end
