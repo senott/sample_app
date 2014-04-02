f2a10a8e36b44b650eaa0d93693cceb4162fa081
Senott <senott@yahoo.com>
2014-04-02 20:12:20 -0300

    11.1.3 Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   
   has_many :microposts, dependent: :destroy
   
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  
   has_secure_password
   
   before_save { self.email = email.downcase }
