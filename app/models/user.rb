0534def8262d56fddaf68a50ad693e697bf7aa97
Senott <senott@yahoo.com>
2014-04-02 21:13:35 -0300

    11.1.5 Followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   
+  has_many :followed_users, through: :relationships, source: :followed
+  
   has_secure_password
   
   before_save { self.email = email.downcase }
@@ -35,6 +37,24 @@ class User < ActiveRecord::Base
     
     # This is preliminary.
     Micropost.where("user_id = ?", id)
+    
+  end
+  
+  def following?(other_user)
+    
+    relationships.find_by(followed_id: other_user.id)
+    
+  end
+  
+  def follow!(other_user)
+    
+    relationships.create!(followed_id: other_user.id)
+    
+  end
+  
+  def unfollow!(other_user)
+    
+    relationships.find_by(followed_id: other_user.id).destroy
     
   end
   
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -19,4 +19,18 @@ describe Relationship do
     
   end
   
+  describe "when followed is not present" do
+    
+    before { relationship.followed_id = nil }
+    it { should_not be_valid }
+    
+  end
+  
+  describe "when follower id is not present" do
+    
+    before { relationship.follower_id = nil }
+    it { should_not be_valid }
+    
+  end
+  
 end
