ccb5950a3720b0b983054d44a31f02aa9940d15c
Senott <senott@yahoo.com>
2014-04-05 13:21:44 -0300

    11.2.4 A working follow button the standard way

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -127,6 +127,20 @@ describe "Authentication" do
             
           end
           
+          describe "visiting the following page" do
+            
+            before { visit following_user_path(user) }
+            it { should have_title('Sign in') }
+            
+          end
+          
+          describe "visiting the followers page" do
+            
+            before { visit followers_user_path(user) }
+            it { should have_title('Sign in') }
+            
+          end
+          
           describe "as wrong user" do
             
             let(:user) { FactoryGirl.create(:user) }
